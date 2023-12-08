db71a9d25ed42a62fe1ab76037646bd338f5e917
Oliver Millard <millardo@hotmail.co.uk>
2023-12-08 01:41:47 +0100

    Landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,13 @@
-import Link from "next/link";
-import {Button} from "@/components/ui/button";
+import {LandingNavBar} from "@/components/landing-navbar";
+import {LandingHero} from "@/components/landing-hero";
+import {LandingContent} from "@/components/landing-content";
 
 const LandingPage = () => {
     return(
-        <div>
-            Landing Page (Unprotected)
-            <div>
-               <Link href="/sign-in">
-                    <Button>
-                        Login
-                    </Button>
-                </Link>
-                <Link href="/sign-up">
-                    <Button>
-                        Register
-                    </Button>
-                </Link>
-            </div>
+        <div className="h-full">
+            <LandingNavBar/>
+            <LandingHero/>
+            <LandingContent/>
         </div>
     );
 }
